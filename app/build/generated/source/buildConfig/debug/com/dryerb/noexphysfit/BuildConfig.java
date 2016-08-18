f2334d90620799206b0e3b828c432290b74d1d0e
DESKTOP-JCBH9L5\dryerb <dryerb825@gmail.com>
2016-08-18 12:26:42 -0400

    first commit

---
+++ app/build/generated/source/buildConfig/debug/com/dryerb/noexphysfit/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.dryerb.noexphysfit;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.dryerb.noexphysfit";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
